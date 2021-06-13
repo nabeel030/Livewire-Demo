5afd5145b62fb8e9a27e42bade8268af75eff589
Nabeel Ahmed <nabeelahmed030@gmail.com>
2021-06-14 00:09:22 +0500

    Logout/Login functionality added

--- app/Http/Livewire/Login.php
+++ app/Http/Livewire/Login.php
@@ -2,17 +2,36 @@
 
 namespace App\Http\Livewire;
 
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Login extends Component
 {
+    public $email = '';
+    public $password = '';
+
     public function render()
     {
         return view('livewire.login');
     }
 
     public function login() {
-        dd('It worked');
+
+        $this->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+        $creds = [
+            'email' => $this->email,
+            'password' => $this->password
+        ];
+
+        if(Auth::attempt($creds))
+            return redirect()->route('home');
+
+        return redirect()->back();
+
     }
 }
---
+++ resources/views/livewire/logout.blade.php
@@ -0,0 +1,5 @@
+<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+    <a class="dropdown-item" wire:click="logout" style="cursor:pointer;">
+        {{ __('Logout') }}
+    </a>
+</div>
